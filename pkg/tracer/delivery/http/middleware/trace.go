eea43233b54aeb513b7885c80e00682da20ca59a
dehwyy <dehwyy@yandex.ru>
2026-03-20 02:13:41 +0300

    [`middleware`]: rename func

--- pkg/tracer/delivery/http/middleware/trace.go
+++ pkg/tracer/delivery/http/middleware/trace.go
@@ -8,7 +8,7 @@ import (
 
 const TraceIDHeaderName = "X-Trace-Id"
 
-func TraceIDHeader(next http.Handler) http.Handler {
+func TraceMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			ctx := r.Context()
